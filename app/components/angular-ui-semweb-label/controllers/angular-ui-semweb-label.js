f324465da686319e381cae54b5b9bbc26b0f4ebd
Clemens Hoffmann <cannelony@gmail.com>
2013-12-16 23:38:44 +0100

    add simple semwebLabel directive (element)

--- app/components/angular-ui-semweb-label/controllers/angular-ui-semweb-label.js
+++ app/components/angular-ui-semweb-label/controllers/angular-ui-semweb-label.js
@@ -1,1 +1,18 @@
 /* angular-ui-semweb-label Controller */
+
+var app = angular.module("app", []);
+
+app.directive("semwebLabel", function() {
+  return {
+    restrict: "E",
+    link: function(scope, element, attributes) {
+      var longUri = element.text();
+      if (longUri.lastIndexOf('#') > -1) {
+        var label = longUri.substr(longUri.lastIndexOf('#') + 1);
+      } else {
+        var label = longUri.substr(longUri.lastIndexOf('/') + 1);
+      }
+      element.text(label);
+    }
+  }
+});
